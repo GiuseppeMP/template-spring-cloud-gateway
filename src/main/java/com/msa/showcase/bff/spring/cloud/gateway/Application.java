cb89c1aea049fa3ea32deacf71a40a7515787d1f
Giuseppe Matheus Pereira <giuseppemath@gmail.com>
2022-07-18 14:05:38 -0300

    (feat) added filters TokenRelay, HeaderRemove, Retries.
    (feat) added actuator

---
+++ src/main/java/com/msa/showcase/bff/spring/cloud/gateway/Application.java
@@ -0,0 +1,14 @@
+package com.msa.showcase.bff.spring.cloud.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+// @EnableDiscoveryCliet
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
